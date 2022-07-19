54c2757b2c3b96f2c2d6981b2dadab505a62272b
GedorN <gedor.neto@gmail.com>
2022-07-18 23:45:56 -0300

    Use dynamic iterface

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
+from click import argument
 import scapy.all as scapy
 from scapy.layers import http
+import optparse
+
+
+def setup():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface currently connectedused to connect to internet")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+            parser.error("[-] interface is required. See --help more more info ")
+    return options
+
+
 
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniff_packet)
@@ -19,4 +32,6 @@ def process_sniff_packet(packet):
                 print(f"\n\n[+] Possible username/password > {load.decode()}\n\n")
                 break
 
-sniff("wlp2s0")
+
+options = setup()
+sniff(options.interface)
